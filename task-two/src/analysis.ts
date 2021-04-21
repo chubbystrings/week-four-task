d4bb698d8bf4ecd378e21d79b384ea44602a12b2
chubbystrings <emeka.okwor@decagon.dev>
2021-04-21 08:59:34 +0100

    eslint errors fixed

--- task-two/src/analysis.ts
+++ task-two/src/analysis.ts
@@ -5,89 +5,79 @@
  * @param {string} outputPath The path to output the analysis
  */
 
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
 interface frequency {
-  [propName: string]: number
+  [propName: string]: number;
 }
 
 interface resultJSON {
-  'valid-domains': string[],
-  totalEmailsParsed: number,
-  totalValidEmails: number,
-  categories: frequency
+  'valid-domains': string[];
+  totalEmailsParsed: number;
+  totalValidEmails: number;
+  categories: frequency;
 }
 
-
-const validateEmail =  (email: string): boolean  => {
+const validateEmail = (email: string): boolean => {
   return /\S+@\S+\.\S+/.test(String(email).toLowerCase());
-}
-
+};
 
 function convertToOutput(path: string): resultJSON {
+  const emails: string = JSON.parse(
+    JSON.stringify(fs.readFileSync(path, 'utf-8')),
+  );
+  let emailArr: string[] = emails.split('\n');
+  emailArr = emailArr.filter((e) => e.includes('@'));
+  let count = 0;
+  let validEmails: string[] = emailArr.filter((e: string) => {
+    count += 1;
+    return validateEmail(e);
+  });
 
-  const emails: string = JSON.parse(JSON.stringify(fs.readFileSync(path, 'utf-8')))
-  let emailArr: string[] = emails.split('\n')
-  emailArr = emailArr.filter((e) => e.includes('@'))
-  let count = 0
-  let validEmails: string[] = emailArr.filter((e: string) =>  {
-  count += 1
-  return validateEmail(e)
-  })
+  const validDomains: string[] = [];
 
-  let validDomains: string[] = [];
-
-  validEmails.forEach(el => {
+  validEmails.forEach((el) => {
     const domain = el.split('@')[1];
     if (!validDomains.includes(domain)) {
-      validDomains.push(domain)
+      validDomains.push(domain);
     }
-  })
+  });
 
-
-  validEmails = validEmails.map((e) => e.split('@')[1])
-  const categories: frequency = validEmails.reduce((tally: frequency, item: string) => {
-    tally[item] = (tally[item] || 0) + 1
-    return tally
-  }, {})
-
+  validEmails = validEmails.map((e) => e.split('@')[1]);
+  const categories: frequency = validEmails.reduce(
+    (tally: frequency, item: string) => {
+      tally[item] = (tally[item] || 0) + 1;
+      return tally;
+    },
+    {},
+  );
 
   const result: resultJSON = {
-    "valid-domains": validDomains,
+    'valid-domains': validDomains,
     totalEmailsParsed: count,
     totalValidEmails: validEmails.length,
-    categories
-  }
+    categories,
+  };
   // let output= JSON.stringify(result, null, 2)
   // console.log(output);
-  return JSON.parse(JSON.stringify(result))
+  return JSON.parse(JSON.stringify(result));
 }
-
-
 
 function syncPaths(str: string): string {
-  const dirPath = path.resolve(__dirname, '..', str)
-  return dirPath
+  const dirPath = path.resolve(__dirname, '..', str);
+  return dirPath;
 }
-
-
 
 function analyseFiles(inputPaths: string[], outputPath: string): void {
-  const outputSynced: string = syncPaths(outputPath)
-  let result = {}
+  const outputSynced: string = syncPaths(outputPath);
+  let result = {};
   inputPaths.forEach((path: string) => {
-    const resolvedPath: string = syncPaths(path)
-    result = convertToOutput(resolvedPath)
-
-  })
-  const json: string = JSON.stringify(result, null, 2)
-  fs.writeFileSync(outputSynced, json)
-
+    const resolvedPath: string = syncPaths(path);
+    result = convertToOutput(resolvedPath);
+  });
+  const json: string = JSON.stringify(result, null, 2);
+  fs.writeFileSync(outputSynced, json);
 }
-
-
-
-
 
 export default analyseFiles;
